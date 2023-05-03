6628ad6080034517bf72f849d2ca58f0eca16d48
Melmon <dylan.drescher@gmail.com>
2023-05-03 22:14:53 +0100

    Pushing my problems to tomorrow.

--- chizuru.py
+++ chizuru.py
@@ -11,17 +11,17 @@
 #  ██║  ██║╚██████╔╝╚██████╔╝╚██████╔╝███████╗
 #  ╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝
 
-# ******************************************************************************************************
-# The following code was adapted from:
-# Author: Sebastian Theiler
-# Accessed from: https://github.com/sebtheiler/tutorials/blob/main/dqn/train_dqn.py
-# Date of last retrieval: 26-04-2023
-# ******************************************************************************************************
+# ****************************************************************************************************** #
+# The following code was adapted from:                                                                   #
+# Author: Sebastian Theiler                                                                              #
+# Accessed from: https://github.com/sebtheiler/tutorials/blob/main/dqn/train_dqn.py                      #
+# Date of last retrieval: 26-04-2023                                                                     #
+# ****************************************************************************************************** #
 
 """This file contains everything needed to run the chizuru-rogue AI."""
 
 from rogue_gym.envs import RogueEnv
-from random import random, randint
+from random import randint
 import tensorflow as tf
 import datetime
 import numpy as np
@@ -49,7 +49,6 @@ EPSILON_START = 1.0
 EPSILON_END = 0.01
 EPSILON_DECAY = 150000
 LEARNING_RATE = 0.00001
-LEARNING_FREQUENCY = 75
 TARGET_UPDATE_FREQUENCY = 750
 PRIORITY_SCALE = 0.7
 
@@ -58,7 +57,9 @@ class ReplayBuffer:
     """ReplayBuffer for storing transitions.
     This implementation was heavily inspired by Fabio M. Graetz's replay buffer
     here: https://github.com/fg91/Deep-Q-Learning/blob/master/DQN.ipynb"""
-    def __init__(self, size=BUFFER_SIZE, input_shape=(21, 79), history_length=4):
+
+    def __init__(self, size=BUFFER_SIZE, input_shape=(21, 79),
+                 history_length=HISTORY_LEN):  # History length for n-step learning
         """
         Arguments:
             size: Integer, Number of stored transitions
@@ -72,9 +73,9 @@ class ReplayBuffer:
         self.current = 0  # index to write to
 
         # Pre-allocate memory
-        self.actions = np.empty(self.size, dtype=np.int32)
+        self.actions = np.empty(self.size, dtype=np.uint8)
         self.rewards = np.empty(self.size, dtype=np.float32)
-        self.frames = np.empty((self.size, self.input_shape[0], self.input_shape[1]), dtype=np.uint8)
+        self.frames = np.empty((self.size, self.input_shape[0], self.input_shape[1]), dtype=np.float32)
         self.terminal_flags = np.empty(self.size, dtype=np.bool)
         self.priorities = np.zeros(self.size, dtype=np.float32)
 
@@ -95,8 +96,9 @@ class ReplayBuffer:
         self.frames[self.current, ...] = frame
         self.rewards[self.current] = reward
         self.terminal_flags[self.current] = terminal
-        self.priorities[self.current] = max(self.priorities.max(initial=0), 1)  # make the most recent experience important
-        self.count = max(self.count, self.current+1)
+        self.priorities[self.current] = max(self.priorities.max(initial=0),
+                                            1)  # make the most recent experience important
+        self.count = max(self.count, self.current + 1)
         self.current = (self.current + 1) % self.size
 
     def get_minibatch(self, batch_size=32, priority_scale=0.0):
@@ -115,18 +117,19 @@ class ReplayBuffer:
             raise ValueError('Not enough memories to get a minibatch')
 
         # Get sampling probabilities from priority list
-        scaled_priorities = self.priorities[self.history_length:self.count-1] ** priority_scale
+        scaled_priorities = self.priorities[self.history_length:self.count - 1] ** priority_scale
         sample_probabilities = scaled_priorities / sum(scaled_priorities)
 
         # Get a list of valid indices
         indices = []
         for i in range(batch_size):
             while True:
-                # Get a random number from history_length to maximum frame written with probabilities based on priority weights
-                index = np.random.choice(np.arange(self.history_length, self.count-1), p=sample_probabilities)
+                # Get a random number from history_length to maximum frame written with probabilities based on
+                # priority weights
+                index = np.random.choice(np.arange(self.history_length, self.count - 1), p=sample_probabilities)
 
-
-                # We check that all frames are from same episode with the two following if statements.  If either are True, the index is invalid.
+                # We check that all frames are from same episode with the two following if statements.  If either are
+                # True, the index is invalid.
                 if index >= self.current >= index - self.history_length:
                     continue
                 if self.terminal_flags[index - self.history_length:index].any():
@@ -138,14 +141,14 @@ class ReplayBuffer:
         states = []
         new_states = []
         for idx in indices:
-            states.append(self.frames[idx-self.history_length:idx, ...])
-            new_states.append(self.frames[idx-self.history_length+1:idx+1, ...])
+            states.append(self.frames[idx - self.history_length:idx, ...])
+            new_states.append(self.frames[idx - self.history_length + 1:idx + 1, ...])
 
         states = np.transpose(np.asarray(states), axes=(0, 2, 3, 1))
         new_states = np.transpose(np.asarray(new_states), axes=(0, 2, 3, 1))
 
         # Get importance weights from probabilities calculated earlier
-        importance = 1/self.count * 1/sample_probabilities[[index - self.history_length for index in indices]]
+        importance = 1 / self.count * 1 / sample_probabilities[[index - self.history_length for index in indices]]
         importance = importance / importance.max()
 
         return (states, self.actions[indices], self.rewards[indices], new_states, self.terminal_flags[indices]), importance, indices
@@ -155,6 +158,7 @@ class ReplayBuffer:
         Arguments:
             indices: Indices to update
             errors: For each index, the error between the target Q-vals and the predicted Q-vals
+            offset
         """
         for i, e in zip(indices, errors):
             self.priorities[i] = abs(e) + offset
@@ -180,6 +184,7 @@ class ReplayBuffer:
 
 class Agent:
     """Contains everything needed to manage the agent."""
+
     def __init__(self, h, w):
         self.h = h
         self.w = w
@@ -189,11 +194,13 @@ class Agent:
 
     def get_action(self, s, e):
         """Agent chooses an action."""
-        rnd_sample = random()
+        rnd_sample = np.random.rand()
 
         if rnd_sample <= e:
-            return randint(0, len(ACTIONS)-1)
-        return self.online_net.predict(s.reshape(-1, 21, 79, HISTORY_LEN))[0].argmax()
+            return randint(0, len(ACTIONS) - 1)
+        reshaped = s.reshape((1, 21, 79, 4))
+        q_vals = self.online_net.predict(reshaped)[0]
+        return q_vals.argmax()
 
     def update_target_network(self):
         """Updates target network with the online network."""
@@ -201,15 +208,16 @@ class Agent:
 
     def learn(self, batch_size, gamma, e, priority_scale=1.0):  # god, I'm so tired.
         """Learns from replays."""
-        (states, actions, rewards, new_states, dones), importance, indices = self.replay_buffer.get_minibatch(batch_size=batch_size, priority_scale=priority_scale)
-        importance = importance ** (1-e)
+        (states, actions, rewards, new_states, dones), importance, indices = self.replay_buffer.get_minibatch(
+            batch_size=batch_size, priority_scale=priority_scale)
+        importance = importance ** (1 - e)
 
         arg_q_max = self.online_net.predict(new_states).argmax(axis=1)
 
         future_q_vals = self.target_net.predict(new_states)
         double_q = future_q_vals[range(batch_size), arg_q_max]
 
-        target_q = tf.cast(rewards, tf.float32) + (gamma * double_q * (1.0 - tf.cast(dones, tf.float32)))
+        target_q = rewards + (gamma * double_q * (1.0 - dones))
 
         with tf.GradientTape() as tape:
             q_values = self.online_net(states)
@@ -230,13 +238,13 @@ class Agent:
 
     def save(self, interval):
         """Saves model at current interval."""
-        save_checkpoint(self.online_net, intr, "online")
+        save_checkpoint(self.online_net, interval, "online")
         save_checkpoint(self.target_net, interval, "target")
 
     def load(self, interval):
         """Loads model at given interval."""
-        self.online_net = load_checkpoint(self.online_net, intr, "online")
-        self.target_net = load_checkpoint(self.target_net, interval, "online")
+        self.online_net = load_checkpoint(self.online_net, interval, "online")
+        self.target_net = load_checkpoint(self.target_net, interval, "target")
 
 
 def create_dueling_dqn(h, w) -> tf.keras.Model:
@@ -250,8 +258,9 @@ def create_dueling_dqn(h, w) -> tf.keras.Model:
                                    kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.))(conv1)
     conv3 = tf.keras.layers.Conv2D(64, (3, 3), strides=1, activation="relu", use_bias=False,
                                    kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.))(conv2)
+    noise = tf.keras.layers.GaussianNoise(0.1)(conv3)
 
-    val, adv = tf.keras.layers.Lambda(lambda ww: tf.split(ww, 2, 3))(conv3)
+    val, adv = tf.keras.layers.Lambda(lambda ww: tf.split(ww, 2, 3))(noise)
 
     val = tf.keras.layers.Flatten()(val)
     val = tf.keras.layers.Dense(1, kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.))(val)
@@ -319,7 +328,7 @@ if __name__ == "__main__":
     env.reset()
     new_state, rew, done, _ = env.step('.')
     agent.replay_buffer.add_experience(9, new_state.gray_image()[0], rew, done)
-    current_game_state = np.repeat(new_state.gray_image().reshape(21, 79, 1), HISTORY_LEN, axis=2)  # with a history of 4
+    current_game_state = np.repeat(new_state.gray_image().reshape(21, 79, 1), HISTORY_LEN, axis=2)  # with a history
 
     # Main processing
     try:
@@ -332,12 +341,13 @@ if __name__ == "__main__":
                 all_rewards.append(rew)
                 interval_rewards.append(rew)
                 all_rewards = all_rewards[-10:]
-                current_game_state = np.append(current_game_state[:, :, 1:], new_state.gray_image().reshape(21, 79, 1), axis=2)
+                current_game_state = np.append(current_game_state[:, :, 1:], new_state.gray_image().reshape(21, 79, 1),
+                                               axis=2)
 
                 agent.replay_buffer.add_experience(act, new_state.gray_image()[0], rew, done)
 
                 # Learning step
-                if step % LEARNING_FREQUENCY == 0 and agent.replay_buffer.count > MIN_REPLAY_SIZE:
+                if agent.replay_buffer.count > MIN_REPLAY_SIZE:
                     loss, _ = agent.learn(BATCH_SIZE, GAMMA, epsilon, PRIORITY_SCALE)
                     all_losses.append(loss)
                     all_losses = all_losses[-100:]
@@ -374,6 +384,5 @@ if __name__ == "__main__":
         print("Exiting~")
         writer.close()
     env.close()
-
 
 # †昇天†
